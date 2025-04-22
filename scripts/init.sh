2c20a6c83df4e1a7471feaadf9f2ea31dc4dc6a3
LeafStative <leafstative@gmail.com>
2025-04-22 15:19:48 +0800

    move tools setup to separate file

--- scripts/init.sh
+++ scripts/init.sh
@@ -80,68 +80,6 @@ check_environment() {
     return 0
 }
 
-download_magiskboot() {
-    local workdir=$1
-    local urls=`curl 'https://api.github.com/repos/topjohnwu/Magisk/releases' | jq -r '.[0].assets[].browser_download_url'`
-    for url in $urls; do
-        if [[ $url =~ 'app-release.apk' || $url =~ Magisk-v.+\.apk ]]; then
-            local apk_url=$url
-            break
-        fi
-    done
-
-    if [[ ! $apk_url ]]; then
-        echo "Failed to find Magisk APK URL."
-        return 1
-    fi
-
-    curl -Lo "$workdir/magisk.apk" $apk_url
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to download Magisk APK."
-        return 1
-    fi
-
-    unzip -jo "$workdir/magisk.apk" 'lib/x86_64/libmagiskboot.so' -d "$workdir"
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to extract libmagiskboot.so from Magisk APK."
-        return 1
-    fi
-
-    cp "$workdir/libmagiskboot.so" tools/magiskboot
-    chmod a+x tools/magiskboot
-}
-
-download_repo() {
-    local workdir=$1
-    curl -Lo "$workdir/repo" https://storage.googleapis.com/git-repo-downloads/repo
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to download repo tool."
-        return 1
-    fi
-
-    cp "$workdir/repo" tools/repo
-    chmod a+x tools/repo
-}
-
-setup_tools() {
-    mkdir setup_workdir
-
-    local result=0
-    if [[ ! -f tools/magiskboot ]]; then
-        download_magiskboot setup_workdir
-        result=$?
-    fi
-    
-    if [[ ! -f tools/repo ]]; then
-        download_repo setup_workdir
-        result=$?
-    fi
-
-    rm -rf setup_workdir
-
-    return $result
-}
-
 check_ksu_variant() {
     local ksu=${1,,}
     case "$ksu" in
@@ -422,14 +360,12 @@ main() {
         exit 1
     fi
 
+    local script_dir=`dirname $(realpath "$0")`
+
     if [[ ! -f 'tools/repo' || ! -f 'tools/magiskboot' ]]; then
         echo "Tools not found, downloading..."
         
-        if [[ ! -d tools ]]; then
-            mkdir tools
-        fi
-
-        setup_tools
+        "$script_dir/lib/setup_tools.sh"
         if [[ $? -ne 0 ]]; then
             echo "Failed to setup tools."
             result=1
